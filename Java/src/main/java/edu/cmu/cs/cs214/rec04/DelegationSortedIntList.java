b8b0ea7275b2d14629464b248e128483793f44f6
edison <yimingy9@illinois.edu>
2024-09-13 17:49:55 -0400

    feat: finish delegation that uses sortedIntList and variable for counting

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
@@ -10,7 +10,49 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class DelegationSortedIntList {
+public class DelegationSortedIntList implements IntegerList {
     // Write your implementation below with API documentation
+    private final SortedIntList sortedIntList;
+    private int totalAdded;
 
+    public DelegationSortedIntList() {
+        sortedIntList = new SortedIntList();
+        totalAdded = 0;
+    }
+
+    public boolean add(int num) {
+        boolean result = sortedIntList.add(num);
+        if (result) totalAdded++;
+        return result;
+    }
+
+    public boolean addAll(IntegerList list) {
+        boolean result = sortedIntList.addAll(list);
+        if (result) totalAdded += list.size();
+        return result;
+    }
+
+    public int get(int index) {
+        return sortedIntList.get(index);
+    }
+
+    public boolean remove(int num) {
+        boolean result = sortedIntList.remove(num);
+        if (result) totalAdded--;
+        return result;
+    }
+
+    public boolean removeAll(IntegerList list) {
+        boolean result = sortedIntList.removeAll(list);
+        if (result) totalAdded -= list.size();
+        return result;
+    }
+
+    public int size() {
+        return sortedIntList.size();
+    }
+
+    public int getTotalAdded() {
+        return totalAdded;
+    }
 }
